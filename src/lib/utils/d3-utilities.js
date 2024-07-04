b249b9402687838163106b291ac2ae4e9ea8f71f
Meng Seong Lee <melee@u82z03s03.42kl.edu.my>
2024-07-04 18:44:27 +0800

    drag and zoom done

--- src/lib/utils/d3-utilities.js
+++ src/lib/utils/d3-utilities.js
@@ -7,23 +7,75 @@ let land;
 let water;
 let projection;
 let path;
-let SCALE;
+let SCALE
+let sens = 0.2;
+
+if (typeof window !== 'undefined') {
+	//hardcoded breakpoints
+	SCALE = window.innerWidth < 768 ? (window.innerWidth < 480 ? 150 : 250) : 325;
+  } else {
+	SCALE = 325; // Default scale for server-side rendering
+}
+let containerWidth;
+let containerHeight;
+
+
+function dragged(event) {
+    let rotation = projection.rotate();
+    const lambda = event.x;
+    const phi = -event.y;
+    projection.rotate([lambda * sens, phi * sens, rotation[2]]); 
+	svg.selectAll("path").attr("d", path);
+//   circle.selectAll("circle").attr('cx', d=>projection(d.coordinates)[0])
+//             .attr('cy', d=>projection(d.coordinates)[1])
+//             .attr("class", isVisible(point.coordinates)? "point":"point hidden" )
+}
+
+const drag = d3.drag()
+	.subject(function() {
+		const r = projection.rotate();	
+		return { x: r[0] / sens, y: -r[1] / sens,z:r[2]};
+	})
+	.on("drag", dragged);
+
+
+
+function zoomed(event) {
+    const {transform} = event;
+    const zoomCenterX = containerWidth / 2;
+    const zoomCenterY = containerHeight / 2;
+    const zoomScale = event.transform.k; // Example: Zoom to scale 2 
+    const newX = containerWidth / 2 - zoomCenterX * zoomScale;
+    const newY = containerHeight / 2 - zoomCenterY * zoomScale; 
+    transform.x = newX;
+    transform.y = newY;
+    // circle.attr("transform", transform )
+    land.attr("transform", transform);
+    water.attr("transform", transform)    
+}
+
+
+
+const zoom = d3.zoom()
+    .filter((event)=>{
+        return event.type === "dblclick" || event.type === "wheel" || event.type === "pinch"
+    })
+	.scaleExtent([1, 6])
+    .on("zoom", zoomed)
 
 export function initialRender(containerRef, svgRef, topoJSONData, theme, colorMode) {
 	console.log(theme)
 	const landColor = colorMode === 'light'? theme.semanticTokens.colors.primary300._light : theme.semanticTokens.colors.primary300._dark;
 	const waterColor = colorMode === 'light'? theme.semanticTokens.colors.homeBoxTurquoise._light : theme.semanticTokens.colors.homeBoxTurquoise._dark;
-	//hardcoded breakpoints
-	SCALE = window.innerWidth < 768 ? (window.innerWidth < 480 ? 150 : 250) : 325;
-	let width = containerRef.current.clientWidth
-    let height = containerRef.current.clientHeight
-	projection = d3.geoOrthographic().clipAngle(90).translate([width/2, height/2]).scale(SCALE)
+	containerWidth = containerRef.current.clientWidth
+    containerHeight = containerRef.current.clientHeight
+	projection = d3.geoOrthographic().clipAngle(90).translate([containerWidth/2, containerHeight/2]).scale(SCALE)
 	path = d3.geoPath(projection)
 
 	svg = d3.select(svgRef.current)
             .attr("width", "100%")
             .attr("height", "100%")
-            .attr("viewBox", `0 0 ${width} ${height}`)
+            .attr("viewBox", `0 0 ${containerWidth} ${containerHeight}`)
 	water = svg.append("g")
 	water
 		.append("path")
@@ -37,5 +89,7 @@ export function initialRender(containerRef, svgRef, topoJSONData, theme, colorMo
 		.data(feature(topoJSONData, topoJSONData.objects.countries).features)
 		.join("path")
 		.attr("d", path);
-		
+
+	svg.call(zoom).call(drag)
+	
 }
