1471c269bc5309c7946bc335bfd359bd876c78a5
Mseong <mseong123@hotmail.com>
2024-07-05 10:31:01 +0800

    done data parsing

--- src/app/geo/page.js
+++ src/app/geo/page.js
@@ -1,13 +1,71 @@
 
 import { promises as fs } from "fs"
 import GeoMap from "@/components/GeoMap"
+import Papa from 'papaparse';
 
-const fileName = "countries-110m.json"
+const topoFileName = "countries-110m.json"
+const datafileName = "SEA_data.csv"
+const regionMapping = {
+  "Thailand ":"SEA",
+  Malaysia:"SEA",
+  Singapore:"SEA",
+  Vietnam:"SEA",
+  Indonesia:"SEA",
+  Philippines:"SEA",
+  China:"EastAsia",
+  "Taiwan ":"EastAsia",
+  Hongkong:"EastAsia",
+
+}
+const coordinatesMapping = {
+  SEA:[115.6628, -2.2180],
+  EastAsia:[106.5348, 38.7946],
+  "Thailand ":[100.9925, 15.8700],
+  Malaysia:[101.9758, 4.2105],
+  Vietnam:[108.2772, 14.0583],
+  Hongkong:[114.1694, 22.3193],
+  "Taiwan ":[120.9605, 23.6978],
+  Philippines:[121.7740, 12.8797],
+  China:[104.1954, 35.8617],
+  Singapore:[103.8198, 1.3521],
+  Indonesia:[113.9213, -0.7893]
+}
+
+function convertData(dataJSON) {
+  //split country if there's more than one location and also filter out Global for now
+  dataJSON.data.forEach(data=>{
+    if (data['Base Where'].includes(',')) 
+      data['Base Where']=data['Base Where'].split(",")
+    else {
+      data['Base Where']=[data['Base Where']]
+    }
+    data['Base Where'] = data['Base Where'].filter(countries=>{
+      return countries !== "Global "
+    })
+  })
+  //convert 'Base Where' to array of objects with region coordinates and country coordinate
+  dataJSON.data.forEach((data)=>{
+    data['Base Where'] = data['Base Where'].map(data=>{
+      return {
+        region:regionMapping[data],
+        regionCoordinates:coordinatesMapping[regionMapping[data]],
+        country:data, 
+        countryCoordinates:coordinatesMapping[data]
+      }
+    })
+  })
+}
+
 
 export default async function Geo() {
-  const content = await fs.readFile(process.cwd()+"/src/data/" + fileName, "utf8")
+  const content = await fs.readFile(process.cwd()+"/src/data/" + topoFileName, "utf8")
   const topoJSONData = JSON.parse(content)
- 
+  const data = await fs.readFile(process.cwd()+"/src/data/" + datafileName, "utf8")
+  const dataJSON = Papa.parse(data, {header:true})
+  convertData(dataJSON)
+  dataJSON.data.forEach(data=>{
+    console.log(data)
+  })
 
   return (
     <GeoMap topoJSONData={topoJSONData}>
